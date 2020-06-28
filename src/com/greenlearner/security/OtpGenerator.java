f92058e4358396ac7413e68c689c52143e7bcdd7
Green Learner <greenlearner01@users.noreply.github.com>
2020-06-28 14:59:22 +0530

    random number generator

---
+++ src/com/greenlearner/security/OtpGenerator.java
@@ -0,0 +1,30 @@
+package com.greenlearner.security;
+
+import java.security.SecureRandom;
+
+/**
+ * @author - GreenLearner(https://www.youtube.com/c/greenlearner)
+ */
+public class OtpGenerator {
+    static char[] getOtp(int len) {
+        // Using numeric values
+        String numbers = "0123456789";
+
+        // Using random method
+//        Random random = new Random();
+        SecureRandom random = new SecureRandom();
+        char[] otp = new char[len];
+
+        for (int i = 0; i < len; i++) {
+            otp[i] = numbers.charAt(random.nextInt(numbers.length()));
+        }
+        return otp;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("You OTP is : ");
+        for (char c : getOtp(6)) {
+            System.out.print(c);
+        }
+    }
+}
---
+++ src/com/greenlearner/security/SecureRandomNumberGeneratorDemo.java
@@ -0,0 +1,40 @@
+package com.greenlearner.security;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+/**
+ * @author - GreenLearner(https://www.youtube.com/c/greenlearner)
+ */
+public class SecureRandomNumberGeneratorDemo {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+//        SecureRandom sc = new SecureRandom();//SHA1PRNG
+
+        SecureRandom sc = SecureRandom.getInstance("Windows-PRNG");
+
+        System.out.println(sc.nextInt(100));
+
+        System.out.println(sc.getAlgorithm());
+        System.out.println(sc.getProvider());
+        System.out.println(sc.nextDouble());
+        System.out.println(sc.nextInt());
+
+        IntStream stream = sc.ints(100); //bounded
+//        sc.ints();//unlimited streams
+        LongStream longStream = sc.longs(100);
+        DoubleStream doubleStream = sc.doubles(100);
+
+        System.out.println(sc.nextInt(100));
+
+        byte[] nextBytes = new byte[100];
+        sc.nextBytes(nextBytes);
+
+        sc.setSeed(nextBytes);
+        sc.nextInt();
+
+    }
+}
